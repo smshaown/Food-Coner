e19759e77d38347432624b5ed6b4c3f1da0c0d6d
smshaown <smshaown65@gmail.com>
2023-05-04 23:32:45 +0600

    done the project

--- src/components/TopRecipe/TopRecipe.jsx
+++ src/components/TopRecipe/TopRecipe.jsx
@@ -22,45 +22,7 @@ const TopRecipe = ({ recipeCard }) => {
   // console.log(recipeCard);
   return (
     <div>
-      {/* <Row xs={1} md={3} classNameName="">
-        <Col>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={recipeCard.recipeItemsImg}
-              classNameName="img-fluid"
-              style={{ width: "100%", height: "300px" }}
-            />
-            <Card.Body>
-              <Card.Title>{recipeName}</Card.Title>
-              <Card.Text>
-                <div>
-                  <h3>Ingredients</h3>
-                  <h4>rating: {recipeCard.rating.number} </h4>
-
-                  <span classNameName="mt-3 li">
-                    {ingredients.map((items, index) => (
-                      <ol>
-                        <li>{items}</li>
-                      </ol>
-                    ))}
-                  </span>
-                </div>
-              </Card.Text>
-              <div>
-                <button
-                  classNameName="btn btn-info mb-2 ml-2"
-                  onClick={handleClick}
-                  disabled={clicked}
-                >
-                  {clicked ? <FcLike /> : <GrFavorite />} Favourite{" "}
-                </button>
-              </div>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row> */}
-
+     
 
           <div className="card" style={{width: "18rem"}}>
             <img src={recipeCard.recipeItemsImg} style={{width: "100%", height: "300px"}} className="card-img-top" alt="..." />
@@ -77,7 +39,7 @@ const TopRecipe = ({ recipeCard }) => {
                     ))}
                   </span>
 
-                  
+
               <button
                   className="btn btn-warning mb-2 ml-2"
                   onClick={handleClick}
