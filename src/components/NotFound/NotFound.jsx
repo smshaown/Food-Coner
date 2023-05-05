39d97d054089b73c1dfd0b2fc1ecfa0819afee86
smshaown <smshaown65@gmail.com>
2023-05-05 20:59:56 +0600

    done the project

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRouteError } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
 
 const NotFound = () => {
     const {error, status} = useRouteError();
@@ -8,7 +8,9 @@ const NotFound = () => {
             <img src="https://i.ibb.co/KDXJNkv/error-404.png"  style={{width: "350px", height: "300px" }} alt="" className='mt-5' />
             <h3 className='fw-bold'> {error.message}</h3>
                
-            <h3 className='text- 2xl font-semibold text-orange-800'>Not Found This Page</h3>  
+            <h3 className='text- 2xl font-semibold text-orange-800'>Not Found This Page</h3>
+
+            <button className="btn btn-danger fw-bold"><Link className="text-decoration-none text-white" to="/">Back to homepage</Link></button>  
           
         </div>
     );
